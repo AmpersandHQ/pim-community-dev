3c8bb8bf5f8d646a1381c6283098f1ecc0b9c557
jmleroux <jmleroux.pro@gmail.com>
2017-02-16 17:38:03 +0100

    PIM-6101: configurations files, DI, scripts

--- src/Pim/Bundle/EnrichBundle/Provider/Field/TextCollectionProvider.php
+++ src/Pim/Bundle/EnrichBundle/Provider/Field/TextCollectionProvider.php
@@ -7,27 +7,13 @@ use Pim\Component\Catalog\AttributeTypes;
 /**
  * Field provider for the Text collection attribute type.
  *
- * Used in the attribute normalizer, during product normalization.
- *
- * @author JM Leroux <jean-marie.leroux@akeneo.com>
+ * @author    JM Leroux <jean-marie.leroux@akeneo.com>
  * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
-class TextCollectionProvider implements FieldProviderInterface
+class TextCollectionProvider extends BaseFieldProvider
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getField($element)
-    {
-       return AttributeTypes::TEXT_COLLECTION;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function supports($element)
-    {
-        return AttributeTypes::TEXT_COLLECTION === $element->getAttributeType();
-    }
+    protected $fields = [
+        AttributeTypes::TEXT_COLLECTION => 'akeneo-text-collection-field',
+    ];
 }
